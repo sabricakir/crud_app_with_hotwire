4702444bc76134b0505d4851cb6706c03715bb4d
Sabri ÇAKIR <computerman353@gmail.com>
2022-03-15 21:17:42 +0300

    update messages counter

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -35,7 +35,8 @@ class MessagesController < ApplicationController
         format.turbo_stream do
           render turbo_stream:[
             turbo_stream.update('new_message',partial:"messages/form",locals:{message:Message.new}),
-            turbo_stream.prepend('messages',partial:"messages/message",locals:{message:@message})
+            turbo_stream.prepend('messages',partial:"messages/message",locals:{message:@message}),
+            turbo_stream.update('message_counter',Message.count)
           ]
         end
         format.html { redirect_to message_url(@message), notice: "Message was successfully created." }
@@ -80,7 +81,10 @@ class MessagesController < ApplicationController
     @message.destroy
 
     respond_to do |format|
-      format.turbo_stream { render turbo_stream: turbo_stream.remove(@message) }
+      format.turbo_stream {render turbo_stream: [
+        turbo_stream.remove(@message),
+        turbo_stream.update('message_counter',Message.count)] 
+        }
       format.html { redirect_to messages_url, notice: "Message was successfully destroyed." }
       format.json { head :no_content }
     end
